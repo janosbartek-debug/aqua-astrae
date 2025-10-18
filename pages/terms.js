d63bc564d7a76221fd45f2c57c8adcc89e64880e
janosbartek-debug <janos.bartek@gmail.com>
2025-10-18 10:56:45 +0200

    Create terms.js

---
+++ pages/terms.js
@@ -0,0 +1,13 @@
+export default function TermsOfUse() {
+  return (
+    <main style={{ padding: '2rem', maxWidth: '700px', margin: '0 auto' }}>
+      <h1>Aqua Astræ – Felhasználási feltételek</h1>
+      <p>Az Aqua Astræ Oraculum spirituális önreflexiós eszköz. Az itt kapott üzenetek nem minősülnek tanácsadásnak.</p>
+      <ul>
+        <li>A szolgáltatást saját felelősségre használhatod.</li>
+        <li>Az oldal fejlesztés alatt áll, hibák előfordulhatnak.</li>
+        <li>Bármikor visszavonhatod az adatok megadását.</li>
+      </ul>
+    </main>
+  );
+}
